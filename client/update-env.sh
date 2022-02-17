2decb7497955cbbf1ee6408a13b9ba9ef89be193
chadcarlson <chad.carlson@platform.sh>
2022-02-17 15:28:44 -0500

    Change 2 things>'

--- client/update-env.sh
+++ client/update-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 API_URL_UNTRIMMED=$(echo $PLATFORM_ROUTES | base64 --decode | jq -r 'to_entries[] | select (.value.id == "api") | .key')
-printf "NEXT_PUBLIC_API_URL=${API_URL_UNTRIMMED::-1}\n" > .env
-printf "PREVIEW_SECRET=mysecret" >> .env
+export NEXT_PUBLIC_API_URL=${API_URL_UNTRIMMED::-1}
+export PREVIEW_SECRET=myNEWsecret
+# printf "NEXT_PUBLIC_API_URL=${API_URL_UNTRIMMED::-1}\n" > .env
+# printf "PREVIEW_SECRET=mysecret" >> .env
